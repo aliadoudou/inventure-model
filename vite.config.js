d6314f8f1a205e2b3367a5b82a6682a48aa4add0
Ali Adoudou <aliadoudou@Alis-MacBook-Air-2.local>
2025-03-02 17:53:05 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/inventure-model/',
+  base: '/inventure-model/'  // This sets the correct base path
 })
